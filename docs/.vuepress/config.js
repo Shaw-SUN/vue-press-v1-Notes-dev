619dcc91310cb17a5edf041ad8b9286a8ea93489
sunwenxiao <2865506149@qq.com>
2022-09-05 14:55:14 +0800

    修改路径名及图片库路由 新增TODO

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,7 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        "@img": "/_resources",
+        "@img": "/docs/_resources",
       },
     },
   },
---
+++ docs/TODO.md
@@ -0,0 +1,7 @@
+# 待解决的 bug
+
+## router 中文及特殊字符路径
+
+encodeURI() 函数用于对 URI 进行编码
+
+> [ encodeURI('/electron/设置透明窗口时的兼容性问题') ] },
